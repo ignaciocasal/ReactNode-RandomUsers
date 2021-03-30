f69848f2514202e52ca1a5061046164928573b61
Ignacio <ignaciocasaldev@gmail.com>
2021-03-30 12:46:10 -0300

    Changed navbar color + Updated screenshot

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import {Container, Icon, Menu} from "semantic-ui-react";
 
 const Header = () => {
     return (
-        <Menu color='violet' inverted stackable style={{borderRadius: 0}}>
+        <Menu inverted stackable style={{borderRadius: 0}}>
             <Container>
                 <Menu.Item>
                     <h2><Icon name='react'/> <Icon name='node js'/> React-Node | Random Users</h2>
